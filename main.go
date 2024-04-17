cb56392ce10bfe4160ffabec36ee50b978a66e06
Alexei Danchenkov <alexeid@scientology.net>
2024-04-16 18:04:52 -0700

    add HEAD method

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,12 +10,17 @@ func main() {
 	httpsRouter := gin.Default()
 	httpsRouter.SetTrustedProxies([]string{"127.0.0.1", "ginjet.test"})
 	httpsRouter.GET("/", homeIndex)
-
-	httpRouter := gin.Default()
-	httpRouter.GET("/*p", func(c *gin.Context) {
-		c.Redirect(302, "https://ginjet.test/"+c.Param("p"))
+	httpsRouter.HEAD("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "")
 	})
 
-	go httpsRouter.RunTLS("ginjet.test:443", "./ssl/ginjet.crt", "./ssl/ginjet.key")
-	httpRouter.Run("ginjet.test:80")
+	// httpRouter := gin.Default()
+	// httpRouter.GET("/*p", func(c *gin.Context) {
+	// 	c.Redirect(302, "https://ginjet.test/"+c.Param("p"))
+	// })
+
+	// go httpsRouter.RunTLS("ginjet.test:443", "./ssl/ginjet.crt", "./ssl/ginjet.key")
+	// httpRouter.Run("ginjet.test:80")
+
+	httpsRouter.RunTLS("ginjet.test:8443", "./ssl/ginjet.crt", "./ssl/ginjet.key")
 }
